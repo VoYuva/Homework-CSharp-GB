99162aea461805c0779147159e4c2d8fa472320b
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-20 14:24:08 +0300

    Add exersize 2-4 from Homework 1

--- HW1/HW1_2-4/Program.cs
+++ HW1/HW1_2-4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+System.Console.Write("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+System.Console.WriteLine($"Max number is {max}");
