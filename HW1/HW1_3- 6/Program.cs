4303764ea572fdf91797dc8fb23c578d1658d8c1
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-20 14:35:19 +0300

    Add exersize 3-6 from Homework 1

--- HW1/HW1_3- 6/Program.cs
+++ HW1/HW1_3- 6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+System.Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    System.Console.WriteLine($"Your number {number} is an even"); //even - четное
+} 
+else
+{
+    System.Console.WriteLine($"Your number {number} is an odd"); //odd - нечётное
+}
