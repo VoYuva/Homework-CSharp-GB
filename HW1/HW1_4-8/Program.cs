44d4db92b050a46b7d9609324309a559ad7f4938
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-20 15:28:23 +0300

    Corrected the check for N

--- HW1/HW1_4-8/Program.cs
+++ HW1/HW1_4-8/Program.cs
@@ -4,7 +4,7 @@ System.Console.Write("Enter your number, which should be greater than 1: ");
 int N = Convert.ToInt32(Console.ReadLine());
 int count = 2; //Так как 1 все равно не чётное и ен будет отображаться, то и смысла начинать с него нет.
 
-while (count <= N && N >= 1)
+while (count <= N && N > 1)
 {
     if (count == 2) System.Console.Write(count); 
     if (count % 2 == 0 && count != 2)//Для красивой записи с запятыми в результате
@@ -14,4 +14,4 @@ while (count <= N && N >= 1)
     count++;
 }
 
-if (N < 1) System.Console.WriteLine("Your number is not correct. Try again with the correct number.");
+if (N <= 1) System.Console.WriteLine("Your number is not correct. Try again with the correct number.");
