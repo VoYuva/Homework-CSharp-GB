f8c3d37eac5635cf1a761c7fc41ddf626325cd69
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-21 21:42:12 +0300

    Make 2-13 exersize from hw2 with two variants of the method

--- HW2/HW2_2-13/Program.cs
+++ HW2/HW2_2-13/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+int FindThirdDigit1 (int num) //Вариант 1
+{
+     if (num < 100)
+    {
+        return -1;
+    }
+
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+    
+    return num % 10;
+}
+
+
+void FindThirdDigit2 (int num) //Вариант 2
+{
+     if (num < 100)
+    {
+        System.Console.WriteLine("Your number has not the third digit");
+    }
+
+    else
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+    
+        System.Console.WriteLine($"The third digit is: {num % 10}");
+    }
+}
+
+
+System.Console.Write("Input your number: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+//System.Console.WriteLine(FindThirdDigit1(num)); // Для вариант 1
+
+FindThirdDigit2(num); // Для варианта 2
