d15b4c15bc157bfcaa2262c48fbd157e5178ff90
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-21 19:45:18 +0300

    Make 1-15 exersize from HW2

--- HW2/HW2_1-10/Program.cs
+++ HW2/HW2_1-10/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа
+
+int FindMiddleDigit(int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        return (num / 10) % 10;
+    }
+    else
+    {
+        return -1;
+    }    
+}
+
+System.Console.Write("Input your number: ");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine(FindMiddleDigit(num));
