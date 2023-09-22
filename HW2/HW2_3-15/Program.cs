d0ce596d2edc7e50c33fe67475920f7ab2bf0147
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-22 12:33:03 +0300

    Make 3-15 exersize from homework 2

--- HW2/HW2_3-15/Program.cs
+++ HW2/HW2_3-15/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
+
+void WeekendOrNot(int num)
+{
+    if (num < 1 | num > 7)
+    {
+        System.Console.WriteLine("This day cannot be in a week");
+    }
+   
+    if (num < 6 && num > 0)
+    {
+        System.Console.WriteLine("This is not weekend");
+    }
+        
+    if (num > 5 && num < 8)
+    {
+        System.Console.WriteLine("This is weekend!");
+    }
+    
+}
+
+System.Console.Write("Input your day's number: ");
+int dayNum = Convert.ToInt32(System.Console.ReadLine());
+
+WeekendOrNot(dayNum);
