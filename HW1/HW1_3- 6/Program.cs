5a775c2b2eb80dca679cf99f542f01fbaf4f888c
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-28 19:05:39 +0300

    some different

--- HW1/HW1_3- 6/Program.cs
+++ HW1/HW1_3- 6/Program.cs
@@ -1,13 +1,32 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
 
-System.Console.Write("Input your number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Input your number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
 
-if (number % 2 == 0)
+// if (number % 2 == 0)
+// {
+//     System.Console.WriteLine($"Your number {number} is an even"); //even - четное
+// } 
+// else
+// {
+//     System.Console.WriteLine($"Your number {number} is an odd"); //odd - нечётное
+// }
+
+class TestClass
 {
-    System.Console.WriteLine($"Your number {number} is an even"); //even - четное
-} 
-else
-{
-    System.Console.WriteLine($"Your number {number} is an odd"); //odd - нечётное
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+        int number = Convert.ToInt32(args[0]);
+
+        if (number % 2 == 0)
+        {
+            System.Console.WriteLine($"Your number {number} is an even"); //even - четное
+        } 
+        else
+        {
+            System.Console.WriteLine($"Your number {number} is an odd"); //odd - нечётное
+        }
+    }
 }
