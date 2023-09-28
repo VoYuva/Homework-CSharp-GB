7fde86c6f4513d46445ea975c711836cb60a4b5f
Voznyuk Yulia <hova-ma@yandex.ru>
2023-09-28 19:06:20 +0300

    ///

--- HW1/HW1_3- 6/Program.cs
+++ HW1/HW1_3- 6/Program.cs
@@ -1,17 +1,4 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
-
-// System.Console.Write("Input your number: ");
-// int number = Convert.ToInt32(Console.ReadLine());
-
-// if (number % 2 == 0)
-// {
-//     System.Console.WriteLine($"Your number {number} is an even"); //even - четное
-// } 
-// else
-// {
-//     System.Console.WriteLine($"Your number {number} is an odd"); //odd - нечётное
-// }
-
 class TestClass
 {
     static void Main(string[] args)
