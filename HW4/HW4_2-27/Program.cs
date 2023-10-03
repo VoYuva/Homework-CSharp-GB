d936081a7bad1eabe683ebdcc3f55621369f0090
Voznyuk Yulia <hova-ma@yandex.ru>
2023-10-03 22:54:08 +0300

    exs 2-27 from hw4

--- HW4/HW4_2-27/Program.cs
+++ HW4/HW4_2-27/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+
+using System;
+
+int SumOfDigitsInNumber (string Number)
+{
+    int num = int.Parse(Number);
+    int result = 0;
+    while (num > 0)
+    {
+        result+=(num%10);
+        num/=10;
+    }
+    return result;
+}
+
+Console.Write("Please, input your number: ");
+string number = Console.ReadLine()!;
+
+Console.WriteLine($"The sum of the digits in your number {number} is equal to {SumOfDigitsInNumber(number)}");
