1ebae1a5299a6340773fab0c8465f4abc489c6ef
Voznyuk Yulia <hova-ma@yandex.ru>
2023-10-04 21:56:46 +0300

    Finished task 2-36 from hw5

--- HW5/HW5_2-36/Program.cs
+++ HW5/HW5_2-36/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+using System;
+public class Exs2_36
+{
+    public static void Main(string[] args)
+    {
+        int[] myArray = new int[10];
+        FillArray100to100(myArray);
+        PrintArray(myArray);
+        Console.WriteLine($" -> {SumElementsWithOddPosition(myArray)}");
+    }
+    public static void FillArray100to100(int[] array)
+    {
+        for(int i=0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(-100, 100);
+        }
+    }
+    public static void PrintArray(int[] array)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write($"[{array[i]}, ");
+            }
+            else if (i == array.Length - 1)
+            {
+                Console.Write($"{array[i]}]");
+            }
+            else
+            {
+                Console.Write($"{array[i]}, ");
+            }
+        }
+    }
+    public static int SumElementsWithOddPosition(int[] array)
+    {
+        int result = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(array[i]%2 != 0)
+            {
+                result+=array[i];
+            }
+        }
+        return result;
+    }
+}
