dc04ab6680f8d52b4e6c33225c3e659c9b1f0f72
Voznyuk Yulia <hova-ma@yandex.ru>
2023-10-04 20:56:23 +0300

    Finished task 1-34 of homework 5

--- HW5/HW5_1-34/Program.cs
+++ HW5/HW5_1-34/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+using System;
+public class Exs1_34
+{
+    public static void Main(string[] args)
+    {
+        int[] myArray = new int[10];
+        FillArray3Digit(myArray);
+        PrintArray(myArray);
+        Console.WriteLine($" -> {CountEvenNumInTheArray(myArray)}");
+    }
+    public static void FillArray3Digit(int[] array)
+    {
+        for(int i=0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(100, 1000);
+        }
+    }
+    public static void PrintArray(int[] array)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write($"[{array[i]}, ");
+            }
+            else if (i == array.Length - 1)
+            {
+                Console.Write($"{array[i]}]");
+            }
+            else
+            {
+                Console.Write($"{array[i]}, ");
+            }
+        }
+    }
+    public static int CountEvenNumInTheArray(int[] array)
+    {
+        int result = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(array[i]%2 == 0)
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
