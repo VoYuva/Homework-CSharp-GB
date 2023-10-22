35013e0483f18104be0ec0cb3d6e6402830b363f
Voznyuk Yulia <hova-ma@yandex.ru>
2023-10-22 13:36:43 +0300

    exs 64 from hw9

--- HW9/HW9_1-64/Program.cs
+++ HW9/HW9_1-64/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+
+public static class Exs64
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(args[0]);
+        Console.WriteLine($"N = {n} -> '{RecFromNto1(n)}'");
+    }
+    public static string RecFromNto1(int n)
+    {
+        if(n == 1)
+        {
+            return "1";
+        }
+        else
+        {
+            return $"{n}, {RecFromNto1(n-1)}";
+        } 
+    }
+}
