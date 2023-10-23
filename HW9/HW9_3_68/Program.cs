5add58d508297a96b012b9eb20dfdab5838c3116
Voznyuk Yulia <hova-ma@yandex.ru>
2023-10-23 09:25:42 +0300

    exs 68 from hw9

--- HW9/HW9_3_68/Program.cs
+++ HW9/HW9_3_68/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using System;
+
+public static class Exs68
+{
+    public static void Main(string[] args)
+    {
+        int m = Convert.ToInt32(args[0]);
+        int n = Convert.ToInt32(args[1]);
+        Console.WriteLine($"m = {m}, n = {n} -> A({m}, {n}) = {AckermanFunction(m, n)}");
+    }
+    public static int AckermanFunction(int m, int n)
+    {
+        if (m == 0) return n + 1;
+        if (n == 0) return AckermanFunction(m - 1, 1);
+        else return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+}
